b203da752f474b72e770453e0f40bdfc8c247c2a
Drew Taylor <drew@taylorspot.com>
2022-02-10 07:13:26 -0600

    Render images, refine display (#7)

--- components/Book.tsx
+++ components/Book.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "../styles/Book.module.scss";
 import Link from "next/dist/client/link";
+import Image from "next/image";
 
 const Book = (props) => {
   return (
     <Link href={props.destination}>
       <div className={styles.container}>
-        <img src={props.image} />
+        <div className={styles.imageContainer}>
+          <Image src={props.image} alt={props.altText} />
+        </div>
         <div className={styles.description}>
           <h4>
             <b>{props.title}</b>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import Header from "../components/Header";
 import Link from "next/dist/client/link";
 import Book from "../components/Book";
 
+import sozImage from "/public/soz.jpg";
+import oshImage from "/public/centennial.png";
+
 export default function Home() {
     return (
       <div className="container">
@@ -14,13 +17,15 @@ export default function Home() {
         <div className="main-div">
           <Book
             destination="/paypal/soz"
-            image="../public/soz.jpg"
+            image={sozImage}
+            altText="Songs of Zion hymn book"
             title="Songs of Zion"
             description="The newest songs written by people you know"
           />
           <Book
             destination="/paypal/osh"
-            image="../public/pbh.png"
+            image={oshImage}
+            altText="Centennial Edition Old School hymn book"
             title="Centennial"
             description="The old and gold"
           />
