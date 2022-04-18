7834e63d825b02b87f51d8cc0116cfdd9b1ad812
Drew Taylor <drew.taylor@perficient.com>
2022-04-17 20:36:23 -0400

    Add login logic, styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import "../styles/globals.scss";
 import Layout from '../components/Layout'
+import { UserContext } from "../lib/context";
+
+import { useUserData } from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <UserContext.Provider value={ userData } >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserContext.Provider>
   )
 }
 
